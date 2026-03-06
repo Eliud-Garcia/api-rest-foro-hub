447ef6d32319b113f96c3d3207f625e3637f31ec
eliud ubuntu <jhonniereliudgarciacapera@gmail.com>
2026-03-06 14:01:17 -0500

    save point

---
+++ src/main/java/com/alura/api_rest_foro/controller/TopicoController.java
@@ -0,0 +1,10 @@
+package com.alura.api_rest_foro.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/topicos")
+public class TopicoController {
+
+}
