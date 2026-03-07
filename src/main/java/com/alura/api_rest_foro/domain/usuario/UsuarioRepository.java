3764221232f52c8f1c4b639d8b8a795680dbf893
eliud ubuntu <jhonniereliudgarciacapera@gmail.com>
2026-03-07 12:57:51 -0500

    validaciones para registrar usuario

--- src/main/java/com/alura/api_rest_foro/domain/usuario/UsuarioRepository.java
+++ src/main/java/com/alura/api_rest_foro/domain/usuario/UsuarioRepository.java
@@ -3,4 +3,5 @@ package com.alura.api_rest_foro.domain.usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    boolean existsByEmail(String email);
 }
--- src/main/java/com/alura/api_rest_foro/service/UsuarioService.java
+++ src/main/java/com/alura/api_rest_foro/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.alura.api_rest_foro.service;
 
+import com.alura.api_rest_foro.domain.ValidacionException;
 import com.alura.api_rest_foro.domain.usuario.Usuario;
 import com.alura.api_rest_foro.domain.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,9 @@ public class UsuarioService {
 
     @Transactional
     public Usuario save(Usuario usuario){
+        if(usuarioRepository.existsByEmail(usuario.getEmail())){
+            throw new ValidacionException("El correo " + usuario.getEmail() + " ya existe!");
+        }
         return usuarioRepository.save(usuario);
     }
 }
