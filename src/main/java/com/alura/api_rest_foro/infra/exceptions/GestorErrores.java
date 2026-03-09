b71035248d6296424fc1ec18959a1260851aaf04
eliud ubuntu <jhonniereliudgarciacapera@gmail.com>
2026-03-09 16:56:52 -0500

    exceptions para autenticacion

--- src/main/java/com/alura/api_rest_foro/infra/exceptions/GestorErrores.java
+++ src/main/java/com/alura/api_rest_foro/infra/exceptions/GestorErrores.java
@@ -2,7 +2,12 @@ package com.alura.api_rest_foro.infra.exceptions;
 
 import jakarta.persistence.EntityNotFoundException;
 import com.alura.api_rest_foro.domain.ValidacionException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -50,6 +55,32 @@ public class GestorErrores {
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<?> gestionarErrorTipoParametro() {
         return ResponseEntity.badRequest().body("El ID proporcionado no es válido. Debe ser un número.");
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity gestionarError400(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity gestionarErrorBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales inválidas");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity gestionarErrorAuthentication() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Falla en la autenticación");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity gestionarErrorAccesoDenegado() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Acceso denegado");
+    }
+
+    /*Error en el servidor*/
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity gestionarError500(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " +ex.getLocalizedMessage());
     }
 
     /*
