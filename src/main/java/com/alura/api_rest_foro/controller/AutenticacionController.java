71744b74c39a8ecb7fce13fbf2858bc8b68cad81
eliud ubuntu <jhonniereliudgarciacapera@gmail.com>
2026-03-09 13:44:51 -0500

    configuracion autenticacion

---
+++ src/main/java/com/alura/api_rest_foro/controller/AutenticacionController.java
@@ -0,0 +1,28 @@
+package com.alura.api_rest_foro.controller;
+
+import com.alura.api_rest_foro.domain.usuario.dto.DatosLogin;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacionController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @PostMapping
+    public ResponseEntity<?> iniciarSesion(@RequestBody @Valid DatosLogin datos){
+        var token = new UsernamePasswordAuthenticationToken(datos.email(), datos.contrasenia());
+        var autenticacion = manager.authenticate(token);
+        //TODO: usar el token para acceso a rutas
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/alura/api_rest_foro/domain/usuario/dto/DatosLogin.java
@@ -0,0 +1,7 @@
+package com.alura.api_rest_foro.domain.usuario.dto;
+
+public record DatosLogin(
+        String email,
+        String contrasenia
+) {
+}
